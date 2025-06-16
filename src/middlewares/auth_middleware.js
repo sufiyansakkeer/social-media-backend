8f7d9aa9aa52d3a1b55aca6c0b9f52a95ca6a14b
sufiyan sakkeer <sufiyansakkeer616@gmail.com>
2025-06-16 10:00:21 +0530

    Fix token extraction order in auth middleware for improved clarity

--- src/middlewares/auth_middleware.js
+++ src/middlewares/auth_middleware.js
@@ -4,8 +4,8 @@ const protect = (req, res, next) => {
   let token = req.headers.authorization;
 
   if (token && token.startsWith("Bearer")) {
-    token = token.split(" ")[1];
     try {
+      token = token.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded.id;
       next();
